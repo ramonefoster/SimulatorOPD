524d8e1e6ebdac437ea0c0d1310514fb2ce33e61
ramonefoster <ramonefoster@gmail.com>
2024-09-17 10:34:15 -0300

    changed to ipyaladin

--- server.py
+++ server.py
@@ -1,30 +1,36 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 
-app = Flask(__name__, template_folder='templates')
-CORS(app, resources={r"/api/*": {"origins": ["http://127.0.0.1:5500", "*", "https://irsa.ipac.caltech.edu"]}})
+import logging
 
-@app.route('/')
+log = logging.getLogger('werkzeug')
+log.disabled = True
+
+DisplayFlask = Flask(__name__, template_folder='templates')
+CORS(DisplayFlask, resources={r"/api/*": {"origins": ["http://127.0.0.1:5500", "*", "https://irsa.ipac.caltech.edu"]}})
+
+@DisplayFlask.route('/')
 def home():
     return render_template('160.html')
 
-@app.route('/iag')
+@DisplayFlask.route('/iag')
 def iag():
     return render_template('iag.html')
 
-@app.route('/aladin')
+@DisplayFlask.route('/aladin')
 def set_aladin():
     return render_template('aladin.html')
 
-@app.route('/set_target', methods=['POST'])
+@DisplayFlask.route('/aladin/set_target', methods=['POST'])
 def set_target():
     global data
     data = request.get_json()
+    print(data)
     # target = data.get('target', 'M 31')  # Default target is M31
     # fov = data.get('fov', 0.5)  # Default fov is 0.5 degrees
     return jsonify({'status': 200})
 
-@app.route('/aladin/get_target', methods=['GET'])
+@DisplayFlask.route('/aladin/get_target', methods=['GET'])
 def get_target():
     global data
     return data
@@ -33,5 +39,5 @@ def get_target():
     # fov = data.get('fov', 0.5)  # Default fov is 0.5 degrees
     # return jsonify({'message': 'Success', 'target': target, 'fov': fov})
 
-if __name__ == '__main__':
-    app.run(port=5500)
+# if __name__ == '__main__':
+#     DisplayFlask.run(port=5500)
--- utils/dso.py
+++ utils/dso.py
@@ -1,4 +1,4 @@
-DSO = ['M57', 'M16', 'NGC 6302', 'NGC 2392', 'M27', 'NGC 3372', 'NGC 6960', 'NGC 2264', 
+DSO = ['M57', 'M16', 'NGC 6302', 'M27', 'NGC 3372', 'NGC 6960', 'NGC 2264', 
        'NGC 253', 'NGC 598', 'NGC 5194', 'NGC 4594', 'NGC 5128', 'NGC 1365', 'NGC 2403', 
        'NGC 6814', 'NGC 3628', 'NGC 1055', 'NGC 2683', 'NGC 6946', 'NGC 1232', 'NGC 1532', 
-       'NGC 772', 'NGC 185']
+       'NGC 772', 'NGC 185', 'Antares', 'Vega']
